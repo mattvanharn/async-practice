68a7387366b450ab0bea2052539852996734154d
Matt Van Harn <mattvanharn@gmail.com>
2022-10-18 22:00:52 -0400

    Update to use async functions

--- app.js
+++ app.js
@@ -5,5 +5,51 @@ let stocks = {
   toppings: ["chocolate", "peanuts"]
 };
 
-let is_shop_open = true;
+let isShopOpen = false;
 
+function time(ms) {
+  return new Promise((resolve, reject) => {
+    if (isShopOpen) {
+      setTimeout(resolve, ms)
+    } else {
+      reject(console.log("Shop is closed"));
+    }
+  });
+}
+
+async function kitchen() {
+  try {
+    await time(2000);
+    console.log(`${stocks.fruits[0]}`);
+
+    await (0000);
+    console.log("Start the production");
+
+    await time(2000);
+    console.log("Cut the fruit");
+
+    await time(1000);
+    console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`);
+
+    await time(1000);
+    console.log("Start the machine");
+
+    await time(2000);
+    console.log(`Ice cream placed on ${stocks.holder[0]}`);
+
+    await time(3000);
+    console.log(`${stocks.toppings[0]} was selected`);
+
+    await time(2000);
+    console.log("Serve ice cream");
+
+  }
+  catch (error) {
+    console.log("Customer left, error");
+  }
+  finally {
+    console.log("Day ended, shop is closed");
+  }
+}
+
+kitchen();
